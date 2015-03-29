9ec074c24493795f694e21be34cca0475a0288a6
Winston Yeung <yeung.winston@gmail.com>
2015-03-29 02:50:59 -0400

    fire hydrant spouts water when destroyed

---
+++ Assets/WaterFountainTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterFountainTimer : MonoBehaviour {
+
+	public float effectDuration = 10f;
+	float totalTime;
+	ParticleEmitter emitter;
+	float originalY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake(){
+		emitter = GetComponent<ParticleEmitter>();
+		originalY = emitter.localVelocity.y;
+		totalTime = effectDuration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		effectDuration -= Time.deltaTime;
+		emitter.localVelocity = new Vector3(0, originalY - Mathf.Lerp(originalY, 0, effectDuration / totalTime), 0);
+
+		if (effectDuration <= 0){
+			DestroyObject (gameObject);
+		}
+	}
+}
